88f4d8b530444ef5cdc52c8b71e2f07cac8d4bca
Salemcity <gemgbeminiyi@gmail.com>
2023-03-22 08:05:25 -0700

    4-main.c

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -1,24 +1,22 @@
 #include "main.h"
 
 /**
- * _isalpha - Return 1 if c is a letter. lowercase or uppercase
+ * main - check the code.
  *
- * @c: The int to print
  * Return: Always 0.
  */
-
-int _isalpha(int c)
-
+int main(void)
 {
+    int r;
 
-	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
-	{
-		return (1);
-	}
-
-	else
-	{
-		return (0);
-	}
-
+    r = _isalpha('H');
+    _putchar(r + '0');
+    r = _isalpha('o');
+    _putchar(r + '0');
+    r = _isalpha(108);
+    _putchar(r + '0');
+    r = _isalpha(';');
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
 }
