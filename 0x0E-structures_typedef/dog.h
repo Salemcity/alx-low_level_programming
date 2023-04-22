912ba583407fa4652737d0433732325754610e34
Salemcity <gemgbeminiyi@gmail.com>
2023-04-22 02:37:17 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef DOG
+#define DOG
 
 	/**
 	 * struct dog - dog class
@@ -9,21 +9,18 @@
 	 *
 	 * Description: Longer description
 	 */
-
-struct dog_h
-
 {
-char *name;
-float age;
-char *owner;
+
+char *name = "Bobby";
+float age = 1.5;
+char *owner = "Gbemzy";
+
 };
 
-void init_dog_h(struct dog_h *d, char *name, float age, char *owner);
-void print_dog(struct dog_h *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
 #endif
