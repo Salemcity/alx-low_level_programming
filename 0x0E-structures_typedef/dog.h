d3a73001466f6efe46b48d7dd9e42d518be491c7
Salemcity <gemgbeminiyi@gmail.com>
2023-04-22 01:46:03 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,23 +2,25 @@
 #define DOG_H
 
 	/**
-	 * struct dog - a dog's basic info
-	 * @name: First group
-	 * @age: Second group
-	 * @owner: Third group
+	 * struct dog - dog class
+	 * @name: Dog name
+	 * @age: Dog age
+	 * @owner: Dog owner
 	 *
 	 * Description: Longer description
 	 */
+
+struct dog_h
 
 {
 char *name;
 float age;
 char *owner;
-}
+};
 
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+void init_dog_h(struct dog_h *d, char *name, float age, char *owner);
+void print_dog(struct dog_h *d);
+typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
